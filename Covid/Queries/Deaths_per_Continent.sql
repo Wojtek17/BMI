e8a87d7e1dc4e7d01bb08911c367a2d98f0c8ab1
Wojtek <112469140+Wojtek17@users.noreply.github.com>
2024-10-08 19:52:39 +0200

    Delete Covid directory

--- Covid/Queries/Deaths_per_Continent.sql
+++
@@ -1,7 +0,0 @@
---Deaths total number per continent
-SELECT continent, SUM(new_deaths)
-FROM coviddeaths
-WHERE continent IS NOT NULL
-GROUP BY continent
-HAVING SUM(new_deaths) >0
-ORDER BY SUM(new_deaths) DESC
